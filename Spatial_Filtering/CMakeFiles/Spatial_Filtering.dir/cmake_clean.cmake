3a5ca3d1fd18d5c2dddefbd259b59ca949f2ddc1
jaybee-117 <unofficialme117@fmail.com>
2022-02-06 16:36:41 +0530

    Spatial filtering

---
+++ Spatial_Filtering/CMakeFiles/Spatial_Filtering.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Spatial_Filtering.dir/Spatial_Filtering.cpp.o"
+  "Spatial_Filtering"
+  "Spatial_Filtering.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Spatial_Filtering.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
