bdd725f9a69f80fad670283f83e382dded59f04d
Jay Bee <jaybee@Jays-MacBook-Air.local>
2022-02-10 17:17:14 +0530

    New code for new mac

--- Histogram/CMakeFiles/Histogram_Equalisation_Matching.dir/cmake_clean.cmake
+++ Histogram/CMakeFiles/Histogram_Equalisation_Matching.dir/cmake_clean.cmake
@@ -1,5 +1,6 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Histogram_Equalisation_Matching.dir/Histogram_Equalisation_Matching.cpp.o"
+  "CMakeFiles/Histogram_Equalisation_Matching.dir/Histogram_Equalisation_Matching.cpp.o.d"
   "Histogram_Equalisation_Matching"
   "Histogram_Equalisation_Matching.pdb"
 )
