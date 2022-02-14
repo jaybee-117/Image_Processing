82006da05fe7073ef3ea33b78e6044d8f86ce1e0
Jay Bee <jaybee@Jays-MacBook-Air.local>
2022-02-14 12:38:15 +0530

    Added switch cases

--- Frequency_Filtering/Frequency_Filtering.cpp
+++ Frequency_Filtering/Frequency_Filtering.cpp
@@ -9,8 +9,7 @@ struct COMPLEX{
     double imag;
 };
 
-
-void displayFFT(COMPLEX** c, int rows, int columns ){
+void displayFFT(COMPLEX** c, int rows, int columns , string fft_name){
     Mat img_fft(rows,columns,CV_8UC1,Scalar(0));
     double mag;
     int i;
@@ -21,10 +20,11 @@ void displayFFT(COMPLEX** c, int rows, int columns ){
             img_fft.at<uchar>(i,j)=255 * mag;
         }
     }
-   imshow("Filtered FFT", img_fft);
+   imshow(fft_name, img_fft);
 
 
 }
+
 int Powerof2(int n,int *m,int *twopm)
 {
    if (n <= 1) {
@@ -167,10 +167,7 @@ int FFT2D(COMPLEX **c,int nx,int ny,int dir)
    return(true);
 }
 
-COMPLEX** showfft(COMPLEX **c, int rows, int columns){
-    double maxi = 0;
-    double mini = 999;
-    double mag;
+COMPLEX** reorganise_quadrant(COMPLEX **c, int rows, int columns){
     int i,j;
 
     Mat img_fft(rows,columns,CV_8UC1,Scalar(0));
@@ -178,13 +175,6 @@ COMPLEX** showfft(COMPLEX **c, int rows, int columns){
    
     for(i=0;i<rows;i++){
         for(j=0;j<columns;j++){
-            mag = sqrt(pow(c[i][j].real,2) + pow(c[i][j].imag,2));
-            if (maxi < mag){
-                maxi = mag;
-            }
-            if (mini> mag){
-                mini = mag;
-            }
             if (j<columns/2 ){
                 quadrant_flip1[i][j+columns/2].real = c[i][j].real;
                 quadrant_flip1[i][j+columns/2].imag = c[i][j].imag;
@@ -195,7 +185,7 @@ COMPLEX** showfft(COMPLEX **c, int rows, int columns){
             }
         }
     }
-    cout<<rows<<" "<<columns<<endl; 
+
     COMPLEX **quadrant_flip= new COMPLEX*[rows];
     for(i=0;i<rows;i++){
         quadrant_flip[i] = new COMPLEX[columns];
@@ -206,75 +196,132 @@ COMPLEX** showfft(COMPLEX **c, int rows, int columns){
         for(j=0;j<columns;j++){
 
             if (i<rows/2 ){
-               quadrant_flip[i+rows/2][j].real = quadrant_flip1[i][j].real;
-               quadrant_flip[i+rows/2][j].imag = quadrant_flip1[i][j].imag;
+                quadrant_flip[i+rows/2][j].real = quadrant_flip1[i][j].real;
+                quadrant_flip[i+rows/2][j].imag = quadrant_flip1[i][j].imag;
             }
             else{
 
                 quadrant_flip[i-rows/2][j].real = quadrant_flip1[i][j].real;
-               quadrant_flip[i-rows/2][j].imag = quadrant_flip1[i][j].imag;
+                quadrant_flip[i-rows/2][j].imag = quadrant_flip1[i][j].imag;
             }
 
         }
     }
-
-    for(i=0;i<rows;i++){
-        for(j=0;j<columns;j++){
-        mag = sqrt(pow(quadrant_flip[i][j].real,2) + pow(quadrant_flip[i][j].imag,2));         
-        img_fft.at<uchar>(i,j)=255 * mag;
-        }
-    }
-
-    cout << maxi << " "<< mini<< endl;
-   imshow("FFT of the image", img_fft);
-   return quadrant_flip;
+    return quadrant_flip;
 }
 
-void lpf(COMPLEX **c,int rows, int columns,int r){
+COMPLEX** lpf(COMPLEX **c, int rows, int columns,int r){
+    COMPLEX** filtered = (COMPLEX**)malloc(rows * sizeof(COMPLEX));
+    for(int i=0;i<rows;i++){
+        filtered[i] = (COMPLEX*)malloc(columns * sizeof(COMPLEX));
+    }
     for(int i=0;i<rows;i++){
         for(int j=0;j<columns;j++){
             if(pow(i-rows/2,2)+pow(j-columns/2,2)<= (r*r)){
-
+                filtered[i][j] = c[i][j];
             }
             else{
-                c[i][j].real=0;
-                c[i][j].imag=0;
+                filtered[i][j].real=0;
+                filtered[i][j].imag=0;
             }
         }
     }
+    return filtered;
 }
 
-void hpf(COMPLEX **c,int rows, int columns,int r){
+COMPLEX** hpf(COMPLEX **c, int rows, int columns,int r){
+    COMPLEX** filtered = (COMPLEX**)malloc(rows * sizeof(COMPLEX));
+    for(int i=0;i<rows;i++){
+        filtered[i] = (COMPLEX*)malloc(columns * sizeof(COMPLEX));
+    }
     for(int i=0;i<rows;i++){
         for(int j=0;j<columns;j++){
             if(pow(i-rows/2,2)+pow(j-columns/2,2)>= (r*r)){
-
+                filtered[i][j] = c[i][j];
             }
             else{
-                c[i][j].real=0;
-                c[i][j].imag=0;
+                filtered[i][j].real=0;
+                filtered[i][j].imag=0;
             }
         }
     }
+    return filtered;
 }
 
-void gaussian_lpf(COMPLEX **c,int rows, int columns,double variance){
+COMPLEX** gaussian_lpf(COMPLEX **c, int rows, int columns,double variance){
+    COMPLEX** filtered = (COMPLEX**)malloc(rows * sizeof(COMPLEX));
+    for(int i=0;i<rows;i++){
+        filtered[i] = (COMPLEX*)malloc(columns * sizeof(COMPLEX));
+    }
+
     for(int i=0;i<rows;i++){
         for(int j=0;j<columns;j++){
             double power = -0.5 * (pow( (i-rows/2)/variance, 2.0 ) + pow( (j-columns/2)/variance, 2.0 ));
-            c[i][j].imag *=exp(power);
-            c[i][j].real *=exp(power);
+            filtered[i][j].imag =c[i][j].imag*exp(power);
+            filtered[i][j].real =c[i][j].real*exp(power);
         }
     }
+    return filtered;
+}
+
+COMPLEX** gaussian_hpf(COMPLEX **c, int rows, int columns,double variance){
+    COMPLEX** filtered = (COMPLEX**)malloc(rows * sizeof(COMPLEX));
+    for(int i=0;i<rows;i++){
+        filtered[i] = (COMPLEX*)malloc(columns * sizeof(COMPLEX));
+    }
+
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            double power = -0.5 * (pow( (i-rows/2)/variance, 2.0 ) + pow( (j-columns/2)/variance, 2.0 ));
+            filtered[i][j].imag =c[i][j].imag*(1-exp(power));
+            filtered[i][j].real =c[i][j].real*(1-exp(power));
+        }
+    }
+    return filtered;
+}
+
+COMPLEX** butterworth_lpf(COMPLEX **c, int rows, int columns, double cutoff){
+    COMPLEX** filtered = (COMPLEX**)malloc(rows * sizeof(COMPLEX));
+    for(int i=0;i<rows;i++){
+        filtered[i] = (COMPLEX*)malloc(columns * sizeof(COMPLEX));
+    }
+    int omega;
+    for(int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++)
+        {
+            omega = sqrt(i*i + j*j);
+            filtered[i][j].real = (cutoff*cutoff*c[i][j].real+omega*cutoff*c[i][j].imag)/(cutoff*cutoff+omega*omega);
+            filtered[i][j].imag = (cutoff*cutoff*c[i][j].imag-omega*cutoff*c[i][j].real)/(cutoff*cutoff+omega*omega);
+        }
+    }
+    return filtered;
+}
+
+COMPLEX** butterworth_hpf(COMPLEX **c, int rows, int columns,double cutoff){
+    COMPLEX** filtered = (COMPLEX**)malloc(rows * sizeof(COMPLEX));
+    for(int i=0;i<rows;i++){
+        filtered[i] = (COMPLEX*)malloc(columns * sizeof(COMPLEX));
+    }
+    int omega;
+    for(int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++)
+        {
+            omega = sqrt(i*i + j*j);
+            filtered[i][j].real = (cutoff*cutoff*c[i][j].real + omega*cutoff*c[i][j].imag - omega*omega*c[i][j].imag)/(cutoff*cutoff + 2*omega*omega + 2*omega*cutoff);
+            filtered[i][j].imag = (cutoff*cutoff*c[i][j].imag - omega*cutoff*c[i][j].real - omega*omega*c[i][j].real)/(cutoff*cutoff + 2*omega*omega + 2*omega*cutoff);
+        }
+    }
+    return filtered;
 }
 
 int main(){
-    Mat img = imread("Input_Images/Lena_gray_512.jpg",0);
+    Mat img = imread("Input_Images/Square.png",0);
     imshow("Sample Image",img);
     int columns = img.cols;
     int rows = img.rows;
     int i,j;
 
+    // Saving the image into a complex image (START)
     COMPLEX** complex_img = (COMPLEX**)malloc(rows * sizeof(COMPLEX));
     for(i=0;i<rows;i++){
         complex_img[i] = (COMPLEX*)malloc(columns * sizeof(COMPLEX));
@@ -286,62 +333,90 @@ int main(){
             complex_img[i][j].imag = 0;
         }
     }
+    // Saving the image into a complex image (END)
 
-    FFT2D(complex_img,rows,columns,1); //forward transform
-    COMPLEX** quadrant = showfft(complex_img, rows,columns);
-    //displayFFT(quadrant,rows,columns);
-    //lpf(quadrant, rows,columns,50);
-    gaussian_lpf(quadrant,rows,columns,20);
-    //hpf(quadrant, rows,columns,50);
-    displayFFT(quadrant,rows,columns);
-    COMPLEX **quadrant_flip= new COMPLEX*[rows];
-    for(i=0;i<rows;i++){
-        quadrant_flip[i] = new COMPLEX[columns];
+    FFT2D(complex_img,rows,columns,1); 
+    displayFFT(complex_img,rows,columns,"Raw FFT");
+
+    //show the FFT of the original Image (START)
+    COMPLEX** img_fft = reorganise_quadrant(complex_img, rows, columns); 
+    displayFFT(img_fft,rows,columns,"Image FFT");
+    //show the FFT of the original Image (END)
+
+    // Filter and show the FFT (START)
+    int filter_no;
+    int radius;
+    int variance;
+    int cutoff;
+    COMPLEX** filtered_img;
+    cout << "Enter the number corrsponding to the following filters:"<< endl;
+    cout <<"1. Low-Pass Filter(Ideal)"<<endl;
+    cout <<"2. High-Pass Filter(Ideal)"<<endl;
+    cout <<"3. Gaussian Low-Pass Filter"<<endl;
+    cout <<"4. Gaussian High-Pass Filter"<<endl;
+    cout <<"5. Butterworth Low-Pass Filter"<<endl;
+    cout <<"6. Butterworth High-Pass Filter"<<endl;
+    cin >> filter_no;
+    switch(filter_no){
+    case 1:
+        cout << "Enter the radius of the LPF: ";
+        cin >> radius;
+        filtered_img = lpf(img_fft,rows,columns,radius);
+        displayFFT(filtered_img,rows,columns,"LPF Filtered FFT");
+        break;
+    case 2:
+        cout << "Enter the radius of the HPF: ";
+        cin >> radius;
+        filtered_img = hpf(img_fft,rows,columns,radius);
+        displayFFT(filtered_img,rows,columns,"HPF Filtered FFT");
+        break;
+    case 3:
+        cout << "Enter the variance of the Gaussian-LPF: ";
+        cin >> variance;
+        filtered_img = gaussian_lpf(img_fft,rows,columns,variance);
+        displayFFT(filtered_img,rows,columns,"Gaussian LPF Filtered FFT");
+        break;
+    case 4:
+        cout << "Enter the variance of the Gaussian-HPF: ";
+        cin >> variance;
+        filtered_img = gaussian_hpf(img_fft,rows,columns,variance);
+        displayFFT(filtered_img,rows,columns,"Gaussian HPF Filtered FFT");
+        break;
+    case 5:
+        cout << "Enter the cutoff of the Butterworth-LPF: ";
+        cin >> cutoff;
+        filtered_img = butterworth_lpf(img_fft,rows,columns,cutoff);
+        displayFFT(filtered_img,rows,columns,"Butterworth LPF Filtered FFT");
+        break;
+    case 6:
+        cout << "Enter the cutoff of the Butterworth-HPF: ";
+        cin >> cutoff;
+        filtered_img = butterworth_hpf(img_fft,rows,columns,cutoff);
+        displayFFT(filtered_img,rows,columns,"Butterworth HPF Filtered FFT");
+        break;
+    default:
+        cout << "Invalid filter number"<< endl;
+        return 0;
     }
-    COMPLEX **quadrant_flip1= new COMPLEX*[rows];
-    for(i=0;i<rows;i++){
-        quadrant_flip1[i] = new COMPLEX[columns];
-    }
-    for(i=0;i<rows;i++){
-        for(j=0;j<columns;j++){
-            if (j<columns/2 ){
-                quadrant_flip1[i][j+columns/2].real = quadrant[i][j].real;
-                quadrant_flip1[i][j+columns/2].imag = quadrant[i][j].imag;
-            }
-            else{
-                quadrant_flip1[i][j-columns/2].real = quadrant[i][j].real;
-                quadrant_flip1[i][j-columns/2].imag = quadrant[i][j].imag;
-            }
-        }
-    }
+    // Filter and show the FFT (END)
 
+    // Show the Raw filtered FFT (START)
+    COMPLEX** raw_filtered_fft = reorganise_quadrant(filtered_img, rows,columns);
+    displayFFT(raw_filtered_fft,rows,columns,"Raw Filtered FFT");
+    // Show the Raw filtered FFT (END)
 
-    for(i=0;i<rows;i++){
-        for(j=0;j<columns;j++){
+    FFT2D(raw_filtered_fft,rows,columns,-1);
 
-            if (i<rows/2 ){
-               quadrant_flip[i+rows/2][j].real = quadrant_flip1[i][j].real;
-               quadrant_flip[i+rows/2][j].imag = quadrant_flip1[i][j].imag;
-            }
-            else{
-
-                quadrant_flip[i-rows/2][j].real = quadrant_flip1[i][j].real;
-               quadrant_flip[i-rows/2][j].imag = quadrant_flip1[i][j].imag;
-            }
-
-        }
-    }
-    displayFFT(quadrant_flip,rows,columns);
-    FFT2D(quadrant_flip,rows,columns,-1);
-
+    //Show the Filtered image (START)
     Mat filtered_image(rows,columns,CV_8UC1,Scalar(0));
-
     for(i=0;i<rows;i++){
         for(j=0;j<columns;j++){
-            filtered_image.at<uchar>(i,j) = quadrant_flip[i][j].real;
+            filtered_image.at<uchar>(i,j) = raw_filtered_fft[i][j].real;
         }
     }
     imshow("Filtered Image",filtered_image);
+    //Show the Filtered image (END)
+
     waitKey(0);
     return 0;
 }
